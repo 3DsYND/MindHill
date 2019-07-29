965a7b16b2406b0b2eb4abee5f153cc2d8e16a3e
3DsYND <3dsYND@gmail.com>
2019-07-29 06:39:07 +0300

    [feat] Add activation of a fan by the player

--- objects/fan/fan.gd
+++ objects/fan/fan.gd
@@ -1,13 +1,10 @@
 extends AnimatedSprite
 
 signal fan_started(times)
+export(Array, int) var times_to_try = [3, 1, 2]
+export(int) var pause_time = 3
 
-
-func _ready():
-	yield(get_tree().create_timer(2), "timeout")
-	trying([3, 1, 2], 3)
-
-func trying(times_array, pause=5):
+func try(times_array, pause):
 	while true:
 		for times in times_array:
 			emit_signal("fan_started", times)
@@ -16,3 +13,6 @@ func trying(times_array, pause=5):
 				play("trying")
 				yield(self, "animation_finished")
 			yield(get_tree().create_timer(pause), "timeout")
+
+func _on_mhButton_action_player_activated():
+	try(times_to_try, pause_time)
